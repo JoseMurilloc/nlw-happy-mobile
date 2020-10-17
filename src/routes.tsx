4575e19f0bf9c7a7d508dba82e3f66aa38b410d4
Jose Murillo <jooseemurillo@gmail.com>
2020-10-17 18:47:27 -0300

    Create header and all pages design

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,15 +5,41 @@ import { createStackNavigator } from '@react-navigation/stack'
 
 import OrphamageMap from './pages/OrphamageMap';
 import OrphanageDetails from './pages/OrphanageDetails';
+import SelectMapPosition from './pages/SelectMapPosition';
+import OrphanageData from './pages/OrphanageData';
+import Header from './components/Header';
 
 const { Navigator, Screen } = createStackNavigator()
 
 const Routes: React.FC = () => {
   return(
     <NavigationContainer>
-      <Navigator screenOptions={{ headerShown: false }}>
+      <Navigator screenOptions={{ headerShown: false, cardStyle: { backgroundColor: '#f2f3f5' } }}>
         <Screen name="OrphanageMap" component={OrphamageMap} />
-        <Screen name="OrphanageDetails" component={OrphanageDetails} />
+        <Screen 
+          name="OrphanageDetails"
+          component={OrphanageDetails} 
+          options={{
+            headerShown: true,
+            header: () => <Header showCancel={false} title="Orfanato" />
+          }}
+        />
+        <Screen
+          name="SelectMapPosition"
+          component={SelectMapPosition} 
+          options={{
+            headerShown: true,
+            header: () => <Header title="Selecione no mapa" />
+          }}
+        />
+        <Screen 
+          name="OrphanageData"
+          component={OrphanageData}
+          options={{
+            headerShown: true,
+            header: () => <Header title="Informe os dados" />
+          }}
+        />
       </Navigator>
     </NavigationContainer>
   );
